f5e0a67fcf1d4a36da35a21ba1b7761b997b0eca
Matthew Smith <guitarbass95@gmail.com>
2024-02-11 00:48:20 -0500

    code is quite messy but I've got some threaded tasks going for updating the display and getting live data

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,9 @@ use sh1106::{prelude::*, Builder};
 
 use rust_kasa::kasa_protocol;
 use std::net::TcpStream;
+use std::{thread, sync::Arc};
+use esp_idf_svc::hal::task::thread::ThreadSpawnConfiguration;
+use std::sync::mpsc;
 use wifi::wifi;
 
 
@@ -45,7 +48,8 @@ struct remote_state {
 //
 fn toggle() -> Result<()>{
     let app_config = CONFIG; 
-    let mut stream = TcpStream::connect(format!("{:}:9999", app_config.target_ip)).ok().unwrap();
+    let mut stream = TcpStream::connect(format!("{:}:9999", app_config.target_ip))?;
+    //let mut stream = TcpStream::connect("10.20.10.155:9999").ok().unwrap();
     kasa_protocol::toggle_relay_by_idx(&mut stream, 0);
     Ok(())
 }
@@ -63,18 +67,85 @@ fn get_allrt() -> Option<Vec<kasa_protocol::Realtime>> {
     let all_rt: Option<Vec<kasa_protocol::Realtime>> = kasa_protocol::get_all_realtime(&mut stream);
     return all_rt
 }
-fn get_ma() -> Option<u32> {
+fn get_ma() -> Option<kasa_protocol::Realtime> {
     let app_config = CONFIG; 
     let mut stream = TcpStream::connect(format!("{:}:9999", app_config.target_ip)).ok()?;
-    let rt = kasa_protocol::get_realtime(&mut stream).unwrap();
-
-    return Some(rt.current_ma)
-}
-
-fn update_display(state: &mut remote_state) {
+    let rt = kasa_protocol::get_realtime(&mut stream);
     
+    return rt
+    //return Some(99)
+}
+
+
+
+fn display_service(i2c: i2c::I2cDriver, rx: mpsc::Receiver<u32>)  -> Result<()>{
+    println!("display_service hit");
+
+
+    let mut display: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
+
+    display.init().unwrap();
+    display.flush().unwrap();
+
+    
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+    
+    Text::with_baseline("Hello world!", Point::zero(), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+
+    //let info = format!("ip: {:?}",(*_wifi).sta_netif().get_ip_info()?.ip);
+    //
+    //Text::with_baseline(&info, Point::new(0, 16), text_style, Baseline::Top)
+    //    .draw(&mut display)
+    //    .unwrap();
+    
+    display.flush().unwrap();
+    
+    loop {
+        
+        match rx.try_recv() {
+            Ok(msg) => {
+                log::info!("got message");
+                display.clear();
+                let ma = format!("current ma: {:?}", msg);
+                Text::with_baseline(&ma, Point::new(0, 26), text_style, Baseline::Top)
+                    .draw(&mut display)
+                    .unwrap();
+                display.flush().unwrap();
+            }
+            _ => (),
+        };
+        
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+    }
 
 }
+
+fn current_service(tx: mpsc::Sender<u32>) {
+    
+    let app_config = CONFIG; 
+    loop {
+
+        let mut stream = TcpStream::connect(format!("{:}:9999", app_config.target_ip)).ok().unwrap();
+        let rt = kasa_protocol::get_realtime(&mut stream);
+        match rt {
+            Some(i) => {
+                //log::info!("sent message");
+                tx.send(i.current_ma);
+        }
+        _ => (),
+        };
+
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+
+    }
+
+}
+
 
 fn main() -> Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -102,11 +173,11 @@ fn main() -> Result<()> {
         }
     };
 
-    let mut rs = remote_state {
-        ps_info: get_sys().unwrap(),
-        realtime: get_allrt().unwrap(),
-        switches: vec![false;5]
-    };
+    //let mut rs = remote_state {
+    //    ps_info: get_sys().unwrap(),
+    //    realtime: get_allrt().unwrap(),
+    //    switches: vec![false;5]
+    //};
 
     let mut button = gpio::PinDriver::input(peripherals.pins.gpio19).unwrap();
     button.set_pull(gpio::Pull::Up).unwrap();
@@ -117,31 +188,35 @@ fn main() -> Result<()> {
 
     let config = i2c::I2cConfig::new().baudrate(400.kHz().into());
     let mut i2c = i2c::I2cDriver::new(i2c, sda, scl, &config)?;
-
-    let mut display: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
-
-    display.init().unwrap();
-    display.flush().unwrap();
     
-    let text_style = MonoTextStyleBuilder::new()
-        .font(&FONT_6X10)
-        .text_color(BinaryColor::On)
-        .build();
-    
-    Text::with_baseline("Hello world!", Point::zero(), text_style, Baseline::Top)
-        .draw(&mut display)
-        .unwrap();
+    let (tx,rx) = mpsc::channel();
+   
+    ThreadSpawnConfiguration {
+         name: Some("mid-lvl-thread\0".as_bytes()),
+         stack_size: 10000,
+         priority: 15,
+         ..Default::default()
+     }
+     .set()
+     .unwrap(); 
 
-    let info = format!("ip: {:?}",(*_wifi).sta_netif().get_ip_info()?.ip);
-    
-    Text::with_baseline(&info, Point::new(0, 16), text_style, Baseline::Top)
-        .draw(&mut display)
-        .unwrap();
-    
-    display.flush().unwrap();
+     let i_thread = thread::Builder::new()
+         .stack_size(5000)
+         .spawn(move || {
+             current_service(tx);
+         });
 
 
-    log::info!("Hello, world!");
+    //thread::spawn(move || {
+    //    button_service(button);
+    //});
+
+    thread::spawn( move || {
+        display_service(i2c, rx);
+    });
+    
+
+    log::info!("Hello, after thread spawn");
 
     loop {
         //if FLAG.load(Ordering::Relaxed) {
@@ -152,16 +227,14 @@ fn main() -> Result<()> {
             std::thread::sleep(std::time::Duration::from_millis(500));
         }
 
-        display.clear();
-        let ma = format!("current ma: {:?}", match get_ma() {
-            Some(c) => c.to_string(),
-            _ => "err".to_string(),
-        });
 
-        Text::with_baseline(&ma, Point::new(0, 26), text_style, Baseline::Top)
-            .draw(&mut display)
-            .unwrap();
-        display.flush().unwrap();
-        std::thread::sleep(std::time::Duration::from_millis(300));
+        //match get_ma() {
+        //    Some(i) => {
+        //        //log::info!("sent message");
+        //        tx.send(i.current_ma);
+        //}
+        //_ => (),
+        //};
+        //std::thread::sleep(std::time::Duration::from_millis(500));
     }
 }
