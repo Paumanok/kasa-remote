8d6aae6e6128bd4abb240fafa2b15419b551d8ff
Matthew Smith <guitarbass95@gmail.com>
2024-03-23 13:10:54 -0400

    added proper button debouncing and broke out button actions from service

--- src/peripheral_util/buttons.rs
+++ src/peripheral_util/buttons.rs
@@ -7,8 +7,50 @@ use esp_idf_svc::hal::gpio;
 use std::net::TcpStream;
 use std::sync::{Arc, Mutex};
 
-pub fn button_service(btn_gpio: Vec<impl gpio::IOPin + 'static>, rs: Arc<Mutex<RemoteState>>) {
+#[derive(Copy)]
+#[derive(Clone)]
+struct Button {
+    last_state: bool,
+}
+
+struct Buttons {
+    btns: [Button; 9],
+}
+
+impl Buttons {
+    pub fn new() -> Self {
+        Self {
+            btns: [ Button{last_state: false} ; 9],
+        }
+    }
+}
+
+fn button_action(btn_idx: usize, rs: &Arc<Mutex<RemoteState>>) {
     let app_config = CONFIG;
+    if btn_idx <= 2 {
+       match rs.lock() {
+            Ok(mut state) => {
+                state.update_from_encoder( match btn_idx {
+                    0 => Direction::CounterClockwise,
+                    1 => Direction::Press,
+                    2 => Direction::Clockwise,
+                    _ => Direction::None,
+                })
+            },
+            _ => (),
+        };
+        // do control button stuff
+    } else {
+        if let Ok(mut stream) = TcpStream::connect(format!("{:}:9999", app_config.target_ip)) {
+            let _res = kasa_protocol::toggle_relay_by_idx(&mut stream, btn_idx - 3);
+        }
+    }
+}
+
+pub fn button_service(btn_gpio: Vec<impl gpio::IOPin + 'static>, rs: Arc<Mutex<RemoteState>>) {
+
+    let mut btns = Buttons::new();
+
     if btn_gpio.len() != 9 {
         return;
     }
@@ -24,31 +66,20 @@ pub fn button_service(btn_gpio: Vec<impl gpio::IOPin + 'static>, rs: Arc<Mutex<R
     for b in &mut buttons {
         b.set_pull(gpio::Pull::Up).unwrap();
     }
-    log::info!("after pull up set");
+
     loop {
-//      for (idx, b) in buttons.iter().enumerate() {
         for idx in 0..buttons.len() {
-            //log::info!("idx at {:}", idx);
             if buttons[idx].is_low() {
-                if idx <= 2 {
-                   match rs.lock() {
-                        Ok(mut state) => {
-                            state.update_from_encoder( match idx {
-                                0 => Direction::CounterClockwise,
-                                1 => Direction::Press,
-                                2 => Direction::Clockwise,
-                                _ => Direction::None,
-                            })
-                        },
-                        _ => (),
-                    };
-                    // do control button stuff
-                } else {
-                    if let Ok(mut stream) = TcpStream::connect(format!("{:}:9999", app_config.target_ip)) {
-                        let _res = kasa_protocol::toggle_relay_by_idx(&mut stream, idx - 3);
-                    }
+                
+                if !btns.btns[idx].last_state {
+                    btns.btns[idx].last_state = true;
+                    log::info!("button {:} pressed", idx); 
+                    
+                    button_action(idx, &rs);
                 }
-                std::thread::sleep(std::time::Duration::from_millis(100));
+                //std::thread::sleep(std::time::Duration::from_millis(100));
+            } else {
+                btns.btns[idx].last_state = false; //wtf logic goes there
             }
         }
 
